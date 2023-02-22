2b2e173b49f7af5b2d8e0dbae8be20bf45a4acbd
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2023-02-23 04:18:05 +0900

    프로그래머스 day4,5
    프로그래머스 day4,5 코딩

---
+++ day4_2.java
@@ -0,0 +1,16 @@
+public class day4_2 {
+    class Solution {
+        public int solution(int n) {
+            int answer = 0;
+            for(int i=1; i<=100; i++){
+                if((n*i)%6 ==0){
+                    answer=(n*i)/6;
+                    break;
+                }else{
+                    continue;
+                }
+            }
+           return answer;
+        }
+    }
+}
--- 미니과제1.java
+++ 미니과제1.java
@@ -1,4 +1,4 @@
-class Main {
+class 미니과제1 {
     public static void main(String[] args) {
         System.out.println("[구구단 출력]");
         for (int i = 1; i < 10; i++) {
