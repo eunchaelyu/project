c8742345364ebf782d1131500f44dd67e274f2b0
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2023-02-22 21:57:08 +0900

    mathpractice9
    수정본

--- mathpractice9.java
+++ mathpractice9.java
@@ -4,7 +4,7 @@ public class mathpractice9 {
     final static int mod = (int) 1e9 + 7;
 
     public static int solution(long n) {
-        // 5c1 * 4c1 * 5c1 * 4c1 * ...
+        // 5c1 * 4c1 * 5c1 * 4c1 * ....
         // 5c1 자리 만큼 * 4c1 자리 만큼 재귀로 계산
         return (int) (recursion(5, (n + 1) / 2) * recursion(4, n / 2) % mod);
     } //n=1일 때 5c1을 1회만 수행하세요
