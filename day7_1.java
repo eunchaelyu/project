f3412d8f1f8a62360e2f42f63279fd237c5ad09c
eunchae <119996957+eunchaelyu@users.noreply.github.com>
2023-02-28 22:08:12 +0900

    day7

---
+++ day7_1.java
@@ -0,0 +1,9 @@
+public class day7_1 {
+    
+import java.util.Arrays;
+class Solution {
+    public String solution(String my_string, String letter) {
+        return my_string.replace(letter,"");
+    }
+}
+}
---
+++ day7_3.java
@@ -0,0 +1,12 @@
+public class day7_3 {
+    class Solution {
+        public int solution(int n, int k) {
+            int answer = 0;
+            if(n>=10){
+                k=k-n/10;
+            }
+            answer=12000*n+2000*k;
+            return answer;
+        }
+    }
+}
